c7d3bdf4a019d0ad572a30eb0ccfffa125532466
leve.ben <3442027850@qq.com>
2020-03-30 13:54:01 +0800

    添加球随鼠标坐标移动的功能

---
+++ Assets/Script/Template/RS_MainBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RS_MainBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateBallPosX();
+    }
+
+    float GetMouseXPos()    //获取鼠标X坐标
+    {
+        Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10)); //反正就要加"+new Vector3(0, 0, 10)"不加不行，我也不知道为什么
+        return MousePos.x;
+    }
+
+    void UpdateBallPosX()  //更新球的X坐标
+    {
+        this.gameObject.transform.position = new Vector3(GetMouseXPos(), this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+    }
+}
