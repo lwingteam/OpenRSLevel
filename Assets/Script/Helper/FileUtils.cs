eb09a5fe65b0fc94bb7501c980392b5c2ac9a29a
qingyiwebt <3211468886@qq.com>
2020-03-30 13:55:23 +0900

    add something

---
+++ Assets/Script/Helper/FileUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileUtils
+{
+    public static string PublicDataPath
+    {
+        get
+        {
+            string result = Application.persistentDataPath;
+            if (result.IndexOf("sdcard") == -1 && result.IndexOf("storage/emulated") == -1)
+            {
+                result = "/storage/emulated/" + result;
+            }
+            return result;
+        }
+    }
+}
